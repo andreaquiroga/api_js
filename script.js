27f2441bee2c055fec21b31b7b2f3e2628ebbeb1
andreaquiroga <andy.afqy@gmail.com>
2021-12-26 16:32:10 +0100

    simple api reading

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
+const api_url = "https://api.covid19tracking.narrativa.com/api/";
+
 window.onload = start();
 function start(){
     let btnLoad =  document.getElementById('btnLoad');
     btnLoad.addEventListener('click', clickButton);
-
 }
 
 async function loadUrl(url){
@@ -11,10 +12,21 @@ async function loadUrl(url){
 }
 
 async function clickButton(){
-    let country = document.getElementById('selectCountry');
+    let country = document.getElementById('selectCountry').value;
+    let date = document.getElementById('inputDate').value;
+    let url = makeUrl(country, date);
     let json = await loadUrl(url);
+    console.log(json);
+    fillData(json, date, country);
 }
 
-function makeUrl(){
+function fillData(json, date, country){
+    document.getElementById('todayConfirmed').innerText = json.dates[date.toString()].countries[country.toString()].today_confirmed;
+    document.getElementById('todayDeaths').innerText = json.dates[date.toString()].countries[country.toString()].today_deaths;
+    document.getElementById('yesterdayConfirmed').innerText = json.dates[date.toString()].countries[country.toString()].yesterday_confirmed;
+    document.getElementById('yesterdayDeaths').innerText = json.dates[date.toString()].countries[country.toString()].yesterday_deaths;
+}
 
+function makeUrl(country, date){
+    return api_url + date + "/country/" + country;
 }
